41b6197dac52a4ed5c0c24f12f037e517a2ef07a
wangkai <836119465@qq.com>
2016-10-02 22:12:20 +0800

    一个线程池的小例子

--- C++Test/main.cpp
+++ C++Test/main.cpp
@@ -1,12 +1,36 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include "Thread.h"
+#include "ThreadPoolExecutor.h"
 
 using namespace std;
 
-
+class R : public Runnable
+{
+public:
+	~R()
+	{
+	}
+	void Run()
+	{
+		printf("Hello World\n");
+	}
+};
 
 int main()
 {
-	cout << "Hello gitfgfHub" << endl;
+	CThreadPoolExecutor * pExecutor = new CThreadPoolExecutor();
+	pExecutor->Init(1, 10, 50);
+	R r;
+	for (int i = 0; i<100; i++)
+	{
+		while (!pExecutor->Execute(&r))
+		{
+		}
+	}
+	pExecutor->Terminate();
+	delete pExecutor;
+	getchar();
+	return 0;
 }
